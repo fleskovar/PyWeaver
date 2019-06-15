293043e82155036eee634a0384a025e9a4b91629
Francisco Leskovar <francisco.leskovar@gmail.com>
2019-06-15 20:10:48 -0300

    Still fixing first package

--- pkg/setup.py
+++ pkg/setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyweaver",
-    version="0.0.1",
+    version="0.0.2",
     author="Example Author",
     author_email="francisco.leskovar@gmail.com",
     description="Web based Python visual programming",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/fleskovar/PyWeaver",
-    download_url = 'https://github.com/fleskovar/PyWeaver/archive/v0.0.1.tar.gz',
+    download_url = 'https://github.com/fleskovar/PyWeaver/archive/v0.0.2.tar.gz',
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
